ed84fc1af398c0781e1ca64eab63095a0ae03369
Subhadeep Chakraborty <subhadeepc2020@gmail.com>
2025-04-27 16:13:14 +0200

    Initial commit

---
+++ src/main/java/dev/subhadeep/paymentserviceapr25/PaymentServiceApr25Application.java
@@ -0,0 +1,13 @@
+package dev.subhadeep.paymentserviceapr25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceApr25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceApr25Application.class, args);
+    }
+
+}
---
+++ src/test/java/dev/subhadeep/paymentserviceapr25/PaymentServiceApr25ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.subhadeep.paymentserviceapr25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApr25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
